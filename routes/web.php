ca3753cb864f5dbf5383af6bef921323f82606c0
System Administrator <root@Ivos-MacBook-Pro.local>
2016-09-28 21:59:57 +0200

    Add joke functionality added

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,13 @@ Route::get('/', 'WelcomeController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+Route::get('home', [
+    'as' => 'home', 'uses' => 'HomeController@index'
+]);
 
 Route::get('/info/{jokeId}', 'HomeController@jokeInfo');
+Route::get('/new', 'HomeController@newJoke');
+Route::get('/create', 'HomeController@create');
 
 Route::get('/edit/{jokeId}', 'HomeController@editJoke');
 Route::get('/delete/{jokeId}', 'HomeController@deleteJoke');
